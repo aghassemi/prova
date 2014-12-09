ae394f3fc279ef3c187c33a69c2c211e6a5bb21f
Ali Ghassemi <aghassemi@google.com>
2014-12-08 18:03:44 -0800

    temporarily turn off writing options

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ socket(function (update) {
   if (!update || !update.message) return;
   if (update.message.start) {
   	var values = JSON.parse(update.message.options);
-  	options.write(values);
+  	//options.write(values);
     run(update.message.url);
   }
 
